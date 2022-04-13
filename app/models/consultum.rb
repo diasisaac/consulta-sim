21d72d9fa709c40376092e6e379992386d9db8f5
Isaac Dias <m.dias.isaac@gmail.com>
2022-04-13 14:14:14 -0300

    funcao para validacao de ddata no model consulta

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -3,6 +3,13 @@ class Consultum < ApplicationRecord
   belongs_to :paciente
 
   validates :data, :horario,presence: true
+  validate valida_data
   validates_associated :medico
   validates_associated :paciente
+
+  def valida_data
+    if data.present? && data < Date.today
+      errors.add(:data, " A data deve ser de hoje ou futura")
+    end
+  end
 end
