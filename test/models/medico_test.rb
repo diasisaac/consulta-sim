0ae3f0dc70abf7463a76474167b58e7068ae252d
Isaac Dias <m.dias.isaac@gmail.com>
2022-04-14 18:42:12 -0300

    Commit de testes de model medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -4,4 +4,36 @@ class MedicoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'nao deveria salvar medicos sem atributos' do
+
+    medico = Medico.new
+    assert_not medico.save
+
+  end
+
+
+  test 'deveria salvar medico com atributos validos' do
+
+    medico = Medico.new nome:"Fernando", email:"joao@gmail.com", cpf:"11111111111",especialidade:"Oftalmologista", crm:'erp8266'
+
+    assert medico.save
+
+  end
+
+  test 'nao deveria salvar medico com cpf invalido' do
+    medico = Medico.new nome:"Fernando", email:"joao@gmail.com", cpf:"11",especialidade:"Oftalmologista", crm:'erp8266'
+
+    assert_not medico.save
+
+  end
+
+  test 'nao deveria salvar medico sem nome' do
+
+    medico = Medico.new email:"joao@gmail.com", cpf:"11111111111",especialidade:"Oftalmologista", crm:'erp8266'
+    assert_not medico.save
+
+  end
+
+
 end
