1219a6970b0097cfecf4e82d7589cb602f504c18
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-18 14:01:50 -0300

    4 testes de model medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "deve salvar Medico corretamente" do
+    medico = Medico.new nome: 'Medico Teste',
+                        cpf: '39775387485',
+                        email: 'medicoteste@gmail.com',
+                        especialidade: 'cardiologista',
+                        crm: 'pe859621'
+    assert medico.save
+  end
+
+
+  test "nao deve salvar Medico com campo email vazio" do
+    medico = Medico.new nome: 'Medico Teste',
+                        cpf: '39775387485',
+                        email: '',
+                        especialidade: 'cardiologista',
+                        crm: 'pe859621'
+
+    assert_not medico.save
+  end
+
+  test "nao deve salvar Medico com campo cpf vazio" do
+    medico = Medico.new nome: 'Medico Teste',
+                        cpf: '',
+                        email: 'medicoteste@gmail.com',
+                        especialidade: 'cardiologista',
+                        crm: 'pe859621'
+
+    assert_not medico.save
+  end
+
+  test "nao deve salvar Medico com campo nome vazio" do
+    medico = Medico.new nome: '',
+                        cpf: '39775387485',
+                        email: 'medicoteste@gmail.com',
+                        especialidade: 'cardiologista',
+                        crm: 'pe859621'
+
+    assert_not medico.save
+  end
+
+
+
 end
