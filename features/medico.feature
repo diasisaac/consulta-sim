f4ae2d72b3f345cdcb5a980e411b292404019d8b
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-19 19:39:21 -0300

    Implementação em medico.feature e medico_steps.rb

--- features/medico.feature
+++ features/medico.feature
@@ -4,13 +4,15 @@ Feature: Medico
   I want to adicionar, remover, mostrar, e atualizar um medico
   So that Eu nao tenho que fazer isso manualmente
 
-  Scenario: criar um novo medico corretamente
+  Scenario: criar medico corretamente
     Given eu estou na pagina de criar novo medico
-    When  eu crio um medico de nome 'Paganini' e cpf '68591237458' e email 'pagalino@gmail.com' e especialidade 'Neurologista' e crm 'lin33652'
-    Then eu vejo que o medico foi salvo 'Paganini'
+    When  eu crio um medico de nome 'Paganini', cpf '68591237458', email 'pagalino@gmail.com', especialidade 'Neurologista' e crm 'lin33652'
+    And eu clico em criar medico 
+    Then eu vejo uma mensagem que informa que o medico foi criado com sucesso
 
 
-  Scenario: criar medico novo invalido
-    When eu crio um medico de cpf '68591237058' e email 'paga@gmail.com' e especialidade 'Oftalmologista' e crm 'ado33652'
-    Then eu vejo uma mensagem de erro de titulo invalido
-
+  Scenario: criar medico com caracteres de crm menor que o permitido
+    Given eu estou na pagina de criar novo medico
+    When  eu crio um medico de nome 'Paganini', cpf '68591237458', email 'pagalino@gmail.com', especialidade 'Neurologista' e crm 'li'
+    And eu clico em criar medico 
+    Then eu vejo uma mensagem de erro que informa que os caracteres de crm sao pequenos
--- features/step_definitions/medico_steps.rb
+++ features/step_definitions/medico_steps.rb
@@ -1,40 +1,27 @@
+
 Given('eu estou na pagina de criar novo medico') do
   # Write code here that turns the phrase above into concrete actions
   visit '/medicos/new'
   expect(page).to have_current_path('/medicos/new')
-
-
 end
 
-
-When('eu crio um medico de nome {string} e cpf {string} e email {string} e especialidade {string} e crm {string}') do |nome, cpf, email, especialidade, crm|
-  # Write code here that turns the phrase above into concrete actions
+When('eu crio um medico de nome {string}, cpf {string}, email {string}, especialidade {string} e crm {string}') do |nome, cpf, email, especialidade, crm|
   fill_in 'medico[nome]', :with => nome
   fill_in 'medico[cpf]', :with => cpf
   fill_in 'medico[email]', :with => email
   fill_in 'medico[especialidade]', :with => especialidade
   fill_in 'medico[crm]', :with => crm
-  click_button 'Create Medico'
 
 end
 
-Then('eu vejo que o medico foi salvo {string}') do |string|
-  # Write code here that turns the phrase above into concrete actions
-  expect(page).to have_content(string)
-  expect(page).to have_current_path(medicos_path + '/' +
-                                      Medico.last.id.to_s)
-end
-
-
-When('eu crio um medico de cpf {string} e email {string} e especialidade {string} e crm {string}') do |cpf, email, especialidade, crm|
-
-  fill_in 'medico[cpf]', :with => cpf
-  fill_in 'medico[email]', :with => email
-  fill_in 'medico[especialidade]', :with => especialidade
-  fill_in 'medico[crm]', :with => crm
+When('eu clico em criar medico') do
   click_button 'Create Medico'
 end
 
-Then('eu vejo uma mensagem de erro de titulo invalido') do
-  assert_selector("Nome can't be blank")
+Then('eu vejo uma mensagem que informa que o medico foi criado com sucesso') do
+  expect(page).to have_content('Medico was successfully created.')
+end
+
+Then('eu vejo uma mensagem de erro que informa que os caracteres de crm sao pequenos') do
+  expect(page).to have_content('Crm is too short (minimum is 6 characters)')
 end
