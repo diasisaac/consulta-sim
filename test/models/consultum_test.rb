f8562328e33dacfe762400042f522919fd84e5c0
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-19 19:03:22 -0300

    4 testes de model consultum_test.rb

--- test/models/consultum_test.rb
+++ test/models/consultum_test.rb
@@ -1,7 +1,99 @@
 require "test_helper"
 
 class ConsultumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Deve salvar uma consulta corretamente" do
+    
+    paciente = Paciente.new nome: 'Paciente Teste',
+                          data_nascimeto: '10-10-1980',
+                          cpf: '11111111112',
+                          email: 'pacienteteste@gmail.com',
+                            endereco_attributes:{ cidade: 'Recife',
+                                                logradouro: 'rua rosa',
+                                                complemento: 'casa',
+                                                bairro: 'Boa Viagem',
+                                                cep: '55555555'}
+    assert paciente.save
+
+    medico = Medico.new nome: 'Medico Teste',
+                        cpf: '39775387485',
+                        email: 'medicoteste@gmail.com',
+                        especialidade: 'cardiologista',
+                        crm: 'pe859621'
+    assert medico.save
+
+    consulta = Consultum.new data: '10-10-2022',
+                          horario: '10:30',
+                          medico_id: medico.id,
+                          paciente_id: paciente.id
+
+    assert consulta.save
+    
+  end
+
+  test "Nao deve salvar uma consulta sem paciente" do
+    
+    medico = Medico.new nome: 'Medico Teste',
+                        cpf: '39775387485',
+                        email: 'medicoteste@gmail.com',
+                        especialidade: 'cardiologista',
+                        crm: 'pe859621'
+    assert medico.save
+
+    consulta = Consultum.new data: '10-10-2022',
+                          horario: '10:30',
+                          medico_id: medico.id
+
+
+    assert_not consulta.save
+    
+  end
+
+  test "Nao deve salvar uma consulta sem medico" do
+    
+    paciente = Paciente.new nome: 'Paciente Teste',
+                          data_nascimeto: '10-10-1980',
+                          cpf: '11111111112',
+                          email: 'pacienteteste@gmail.com',
+                            endereco_attributes:{ cidade: 'Recife',
+                                                logradouro: 'rua rosa',
+                                                complemento: 'casa',
+                                                bairro: 'Boa Viagem',
+                                                cep: '55555555'}
+    assert paciente.save
+
+    consulta = Consultum.new data: '10-10-2022',
+                          horario: '10:30',
+                          paciente_id: paciente.id
+
+    assert_not consulta.save
+    
+  end
+
+  test "Nao deve salvar uma consulta sem data" do
+    
+    paciente = Paciente.new nome: 'Paciente Teste',
+                          data_nascimeto: '10-10-1980',
+                          cpf: '11111111112',
+                          email: 'pacienteteste@gmail.com',
+                            endereco_attributes:{ cidade: 'Recife',
+                                                logradouro: 'rua rosa',
+                                                complemento: 'casa',
+                                                bairro: 'Boa Viagem',
+                                                cep: '55555555'}
+    assert paciente.save
+
+    medico = Medico.new nome: 'Medico Teste',
+                        cpf: '39775387485',
+                        email: 'medicoteste@gmail.com',
+                        especialidade: 'cardiologista',
+                        crm: 'pe859621'
+    assert medico.save
+
+    consulta = Consultum.new horario: '10:30',
+                             medico_id: medico.id,
+                             paciente_id: paciente.id
+
+    assert_not consulta.save
+    
+  end
 end
