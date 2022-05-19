4baa08af550eda3f1749f70265c34474b501761f
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-19 10:26:34 -0300

    4 testes de model endereco_test.rb

--- test/models/endereco_test.rb
+++ test/models/endereco_test.rb
@@ -1,7 +1,65 @@
 require "test_helper"
 
 class EnderecoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Deve salvar um endereco corretamente" do
+    
+    
+    paciente = Paciente.new nome: 'Paciente Teste',
+                          data_nascimeto: '10-10-1980',
+                          cpf: '11111111112',
+                          email: 'pacienteteste@gmail.com',
+                            endereco_attributes:{ cidade: 'Recife',
+                                                logradouro: 'rua rosa',
+                                                complemento: 'casa',
+                                                bairro: 'Boa Viagem',
+                                                cep: '55555555'}
+    assert paciente.save
+  end
+
+  test "Nao deve salvar um endereco sem cep" do
+    
+    
+    paciente = Paciente.new nome: 'Paciente Teste',
+                          data_nascimeto: '10-10-1980',
+                          cpf: '11111111112',
+                          email: 'pacienteteste@gmail.com',
+                            endereco_attributes:{ cidade: 'Recife',
+                                                logradouro: 'rua rosa',
+                                                complemento: 'casa',
+                                                bairro: 'Boa Viagem',
+                                                cep: ''}
+    assert_not paciente.save
+  end
+
+  test "Nao deve salvar um endereco com bairro menor que 3 caracter" do
+    
+    
+    paciente = Paciente.new nome: 'Paciente Teste',
+                          data_nascimeto: '10-10-1980',
+                          cpf: '11111111112',
+                          email: 'pacienteteste@gmail.com',
+                            endereco_attributes:{ cidade: 'Recife',
+                                                logradouro: 'rua rosa',
+                                                complemento: 'casa',
+                                                bairro: 'B',
+                                                cep: '55555555'}
+    assert_not paciente.save
+  end
+
+  test "Nao deve salvar um endereco com sem o campo cidade" do
+    
+    
+        paciente = Paciente.new nome: 'Paciente Teste',
+                              data_nascimeto: '10-10-1980',
+                              cpf: '11111111112',
+                              email: 'pacienteteste@gmail.com',
+                                endereco_attributes:{ cidade: '',
+                                                    logradouro: 'rua rosa',
+                                                    complemento: 'casa',
+                                                    bairro: 'Bpa Viagem',
+                                                    cep: '55555555'}
+    assert_not paciente.save
+  end
+
+  
 end
